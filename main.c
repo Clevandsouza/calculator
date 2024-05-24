f46e1efef8893ab54847c020304efc02fa674683
un <un@gmail.com>
2024-05-24 10:45:39 +0530

    -all

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int num1,num2;
+   char oper;
+   printf("enter the operation\n");
+   scanf("%c",&oper);
+   printf("enter the num 1\n");
+   scanf("%d",&num1);
+   printf("enter the num 2\n");
+   scanf("%d",&num2);
+
+
+   switch(oper)
+   {
+        case '+':
+                    printf("sum of numbers is %d\n",num1+num2);
+                    break;
+        default :
+                    printf("invalid\n");
+                    break;
+   }
+    return 0;
+}
